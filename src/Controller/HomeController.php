2ccd70dc3135f4a4b7a4756b6e24376e1a377fd2
Анастасия Андреева <noriaki_rr@MacBook-Air-Anastasia-2.local>
2023-09-21 12:08:32 +0300

    fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,6 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): JsonResponse
     {
-        // return $this->render('home/index.html.twig', [
-        //     'controller_name' => 'HomeController',
-        // ]);
         return $this->json([]);
     }
 }
